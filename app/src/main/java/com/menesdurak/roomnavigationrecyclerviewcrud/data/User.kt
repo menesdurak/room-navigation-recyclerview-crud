5293b70720cd50a8ffe894a5099ac95605814bff
menesdurak <m.enes.durak@gmail.com>
2022-11-29 20:00:55 +0300

    Entity class created

---
+++ app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/data/User.kt
@@ -0,0 +1,13 @@
+package com.menesdurak.roomnavigationrecyclerviewcrud.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val firtName: String,
+    val lastName: String,
+    val age: Int
+)
