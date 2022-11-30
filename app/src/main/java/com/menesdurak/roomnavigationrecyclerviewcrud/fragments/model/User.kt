c873f264250deba166bd42829758bdab7b6ee9b3
menesdurak <m.enes.durak@gmail.com>
2022-11-30 14:58:30 +0300

    Maked User model parcelable and created argument in navigation

--- app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/fragments/model/User.kt
+++ app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/fragments/model/User.kt
@@ -1,8 +1,11 @@
 package com.menesdurak.roomnavigationrecyclerviewcrud.fragments.model
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 @Entity(tableName = "user_table")
 data class User(
     @PrimaryKey(autoGenerate = true)
@@ -10,4 +13,4 @@ data class User(
     val firtName: String,
     val lastName: String,
     val age: Int
-)
+): Parcelable
