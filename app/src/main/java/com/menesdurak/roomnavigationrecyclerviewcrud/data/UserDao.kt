7f1caf9d4c532431389111eddd3faea115cf141d
menesdurak <m.enes.durak@gmail.com>
2022-11-30 15:31:30 +0300

    Update operation complete

--- app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/data/UserDao.kt
+++ app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/data/UserDao.kt
@@ -5,6 +5,7 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
 import com.menesdurak.roomnavigationrecyclerviewcrud.fragments.model.User
 
 @Dao
@@ -12,6 +13,9 @@ interface UserDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun addUser(user: User)
+
+    @Update
+    suspend fun updateUser(user: User)
 
     @Query("SELECT * FROM user_table ORDER BY id ASC")
     fun readAllData(): LiveData<List<User>>
--- app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/fragments/update/UpdateFragment.kt
@@ -1,12 +1,20 @@
 package com.menesdurak.roomnavigationrecyclerviewcrud.fragments.update
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.menesdurak.roomnavigationrecyclerviewcrud.R
 import com.menesdurak.roomnavigationrecyclerviewcrud.databinding.FragmentUpdateBinding
+import com.menesdurak.roomnavigationrecyclerviewcrud.fragments.model.User
+import com.menesdurak.roomnavigationrecyclerviewcrud.viewmodel.UserViewModel
 
 class UpdateFragment : Fragment() {
     private var _binding: FragmentUpdateBinding? = null
@@ -14,6 +22,8 @@ class UpdateFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val args by navArgs<UpdateFragmentArgs>()
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,9 +33,39 @@ class UpdateFragment : Fragment() {
         _binding = FragmentUpdateBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
         binding.updateFirstNameEt.setText(args.currentUser.firstName)
         binding.updateLastNameEt.setText(args.currentUser.lastName)
         binding.updateAgeEt.setText(args.currentUser.age.toString())
+
+        binding.btnUpdate.setOnClickListener {
+            updateItem()
+        }
         return view
+    }
+
+    private fun updateItem() {
+        val firstName = binding.updateFirstNameEt.text.toString()
+        val lastName = binding.updateLastNameEt.text.toString()
+        val age = Integer.parseInt(binding.updateAgeEt.text.toString())
+
+        if((inputCheck(firstName, lastName, binding.updateAgeEt.text))) {
+            //Create User object
+            val updatedUser = User(args.currentUser.id, firstName, lastName, age)
+            //Update current User
+            mUserViewModel.updateUser(updatedUser)
+            Toast.makeText(requireContext(), "Updated succesfully!", Toast.LENGTH_SHORT).show()
+            //Navigate Back
+            findNavController().navigate(R.id.action_updateFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(),
+                "Please fill out all places!",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 }
