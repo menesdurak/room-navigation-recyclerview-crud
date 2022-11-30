0c888c0efeb87e98896615849a65da809c9cb651
menesdurak <m.enes.durak@gmail.com>
2022-11-30 09:42:49 +0300

    Action bar name appearance changed

--- app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/MainActivity.kt
+++ app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/MainActivity.kt
@@ -2,10 +2,20 @@ package com.menesdurak.roomnavigationrecyclerviewcrud
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //For displaying fragments name in the actionbar
+        setupActionBarWithNavController(navController)
     }
 }
