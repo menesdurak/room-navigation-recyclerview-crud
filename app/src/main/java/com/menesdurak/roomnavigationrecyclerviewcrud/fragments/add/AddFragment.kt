e221841fd7e716b54929f8d948b232eb39a85ad6
menesdurak <m.enes.durak@gmail.com>
2022-11-30 11:55:41 +0300

    Input data operation added to AddFragment.kt

--- app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/fragments/add/AddFragment.kt
+++ app/src/main/java/com/menesdurak/roomnavigationrecyclerviewcrud/fragments/add/AddFragment.kt
@@ -1,18 +1,26 @@
 package com.menesdurak.roomnavigationrecyclerviewcrud.fragments.add
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.menesdurak.roomnavigationrecyclerviewcrud.R
+import com.menesdurak.roomnavigationrecyclerviewcrud.data.User
+import com.menesdurak.roomnavigationrecyclerviewcrud.data.UserViewModel
 import com.menesdurak.roomnavigationrecyclerviewcrud.databinding.FragmentAddBinding
 
 class AddFragment : Fragment() {
     private var _binding: FragmentAddBinding? = null
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var mUserViewModel: UserViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,7 +29,37 @@ class AddFragment : Fragment() {
     ): View? {
         _binding = FragmentAddBinding.inflate(inflater, container, false)
         val view = binding.root
+
+        mUserViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        binding.btnAdd.setOnClickListener {
+            insertDataToDatabase()
+        }
         return view
+    }
+
+    private fun insertDataToDatabase() {
+        val firstName = binding.addFirstNameEt.text.toString()
+        val lastName = binding.addLastNameEt.text.toString()
+        val age = binding.addAgeEt.text
+
+        if (inputCheck(firstName, lastName, age)) {
+            //Create User Object
+            val user = User(0, firstName, lastName, Integer.parseInt(age.toString()))
+            //Add data to database
+            mUserViewModel.addUser(user)
+            Toast.makeText(requireContext(), "Succesfully added!", Toast.LENGTH_SHORT).show()
+            //Navigate Back
+            findNavController().navigate(R.id.action_addFragment_to_listFragment)
+        } else {
+            Toast.makeText(requireContext(),
+                "Please fill out all fields!",
+                Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun inputCheck(firstName: String, lastName: String, age: Editable): Boolean {
+        return !(TextUtils.isEmpty(firstName) && TextUtils.isEmpty(lastName) && age.isEmpty())
     }
 
     override fun onDestroyView() {
